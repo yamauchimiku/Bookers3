647336c011e36ed47828c1c409613d485564c432
yamauchimiku <yamamochi7171@gmail.com>
2021-12-06 13:04:55 +0000

    [add] user show index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,6 +29,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
     @user = User.find(params[:id])
+    # @userに紐付けられた投稿だけを表示
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+
   end
 
   # ユーザー情報のストロングパラメーター
